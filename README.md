ad5436d38d6945db8fd0a18968cfcefb60a0f517
Pavel Politov <politov@meetsidekick.com>
2020-11-27 23:54:41 +0400

    slack

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pull_requests_action
+Installation:
+      - name: Checkout
+        uses: actions/checkout@v2
+        with:
+          repository: ppolitov/pull_requests_action
+          path: actions/pulls
+
+      - name: Node Modules
+        shell: bash
+        working-directory: actions/pulls
+        run: |
+          npm install
+
+      - name: Check Pull Requests
+        uses: ./actions/pulls
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const { IncomingWebhook } = require('@slack/webhook');
 
 async function run() {
   try {
     const token = core.getInput('token');
+    const slackToken = core.getInput('slack_token');
+
     const { payload } = github.context;
     const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/');
-    //const [owner, repo] = payload.repository.full_name.split('/')
     console.log(`owner: ${owner} repo: ${repo}`);
 
     const octokit = github.getOctokit(token);
     const { data: pulls } = await octokit.pulls.list({owner, repo});
-    console.log('Open pull_requests:', pulls.filter(p => p.state === 'open'));
+    console.log('Open pull_requests:',
+      pulls.filter(p => p.state === 'open').length);
 
     const problemPulls = [];
     for (let pull of pulls) {
@@ -23,7 +26,21 @@ async function run() {
         }
       }
     }
-    console.log('Not mergeable pr:', problemPulls);
+
+    if (problemPulls.length == 0) {
+      return;
+    }
+
+
+    let text = `Conflicts detected in following pull requests:`;
+    for (let pull of problemPulls) {
+      text += `
+        <${pull.html_url}|#${pull.number}> ${pull.title`;
+    }
+
+    const slackUrl = `https://hooks.slack.com/services/${slackToken}`;
+    const webhook = new IncomingWebhook(url);
+    await webhook.send({text});
 
   } catch (error) {
     core.setFailed(error.message);
