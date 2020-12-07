b67b2a7c6aafbc584ee06f29a7a19553ae4987ba
Pavel Politov <politov@meetsidekick.com>
2020-12-07 11:07:23 +0400

    branch

--- index.js
+++ index.js
@@ -2,30 +2,58 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const { IncomingWebhook } = require('@slack/webhook');
 
+/**
+ * @param {number} ms
+ * @return {Promise}
+ */
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * @param {Object} octokit API
+ * @param {String} owner
+ * @param {String} repo
+ * @param {number} pull_number
+ * @return {Object} pull_request
+ */
+async function getPullRequest(octokit, owner, repo, pull_number) {
+  for (let i = 0; i < 10; i++) {
+    const { data: pr } = await octokit.pulls.get({owner, repo, pull_number});
+    // Null while github checks the pull request
+    if (pr.mergeable !== null) {
+      return pr;
+    }
+    await timeout(1000);
+  }
+}
+
 async function run() {
   try {
+    const branch = core.getInput('branch');
     const token = core.getInput('token');
     const slackToken = core.getInput('slack_token');
 
     const { payload } = github.context;
     const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/');
-    console.log(`owner: ${owner} repo: ${repo}`);
+    console.log(`Inputs: branch:${branch} owner:${owner} repo:${repo}`);
 
     const octokit = github.getOctokit(token);
     const { data: pulls } = await octokit.pulls.list({owner, repo});
-    console.log('Open pull_requests:',
-      pulls.filter(p => p.state === 'open').length);
+
+    const openPulls = pulls.filter(pull =>
+      pull.state === 'open' && !pull.draft && pull.base.ref === branch);
+    console.log('Open pull_requests:', openPulls.length);
 
     const problemPulls = [];
-    for (let pull of pulls) {
-      if (pull.state === 'open' && !pull.draft) {
-        const { data: pr } = await octokit.pulls.get(
-          {owner, repo, pull_number: pull.number});
-        if (!pr.rebaseable) {
-          problemPulls.push(pr);
-        }
+    for (let pull of openPulls) {
+      const pr = await getPullRequest(octokit, owner, repo, pull.number);
+      if (pr && pr.rebaseable === false) { // can be null
+        problemPulls.push(pr);
       }
     }
+
+    console.log('Not rebaseable pull requests:', problemPulls.number);
 
     if (problemPulls.length == 0) {
       return;
